ec4999ec3001d85034b462a8bb64af2244679332
Vladimir Krumshtein <krum@vk-cv.clustervision.com>
2016-10-24 12:44:28 +0200

    added flock test

--- flock.py
+++ flock.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import fcntl,time
+
+
+with open("lock") as f:
+    print("locking")
+    fcntl.flock(f,fcntl.LOCK_EX)
+    print("locked")
+    print("sleeping 10 seconds")
+    time.sleep(10)
